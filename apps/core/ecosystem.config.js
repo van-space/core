b5a38195c045538fb16607063c009182c6dadd66
huangyan321 <1604549268@qq.com>
2024-06-01 23:42:47 +0800

    feat: init again

---
+++ apps/core/ecosystem.config.js
@@ -0,0 +1,19 @@
+const env = require('dotenv').config().parsed;
+module.exports = {
+  apps: [
+    {
+      name: 'van-space-server',
+      script: 'dist/main.js',
+      autorestart: true,
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: false,
+      // instances: 1,
+      // max_memory_restart: env.APP_MAX_MEMORY || '150M',
+      env: {
+        NODE_ENV: 'production',
+        ...env,
+      },
+    },
+  ],
+};
