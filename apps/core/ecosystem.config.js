e84af05fa3abbdfcff44557aa7525b3202e4d30c
huangyan321 <1604549268@qq.com>
2024-06-19 22:13:13 +0800

    chore: update script

--- apps/core/ecosystem.config.js
+++ apps/core/ecosystem.config.js
@@ -1,19 +1,13 @@
-const env = require('dotenv').config().parsed;
 module.exports = {
   apps: [
     {
-      name: 'van-space-server',
-      script: 'dist/src/main.js',
+      name: 'mx-server',
+      script: 'index.js',
       autorestart: true,
-      instances: 'max',
       exec_mode: 'cluster',
       watch: false,
-      // instances: 1,
-      // max_memory_restart: env.APP_MAX_MEMORY || '150M',
-      env: {
-        NODE_ENV: 'production',
-        ...env,
-      },
+      instances: 2,
+      max_memory_restart: '230M',
     },
   ],
 };
