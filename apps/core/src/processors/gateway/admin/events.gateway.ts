50d1b0812259a13d69e0aa85401d0084a4713d87
huangyan321 <1604549268@qq.com>
2024-06-23 03:33:26 +0800

    feat: log module

--- apps/core/src/processors/gateway/admin/events.gateway.ts
+++ apps/core/src/processors/gateway/admin/events.gateway.ts
@@ -1,18 +1,17 @@
-/**
- * 实现了 WebSocket 连接的认证、连接管理和通知发送功能
- */
+import { Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import {
   GatewayMetadata,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WsException,
 } from '@nestjs/websockets';
+import SocketIO, { Socket } from 'socket.io';
 import { AuthService } from '../../../modules/auth/auth.service';
 import { BaseGateway } from '../base.gateway';
 import { EventTypes, NotificationTypes } from '../events.types';
-import SocketIO from 'socket.io';
 @WebSocketGateway<GatewayMetadata>({ namespace: 'admin' })
 export class AdminEventsGateway
   extends BaseGateway
@@ -23,12 +22,13 @@ export class AdminEventsGateway
     private readonly authService: AuthService,
   ) {
     super();
+    this.bindStdOut();
   }
-
   async authFailed(client: SocketIO.Socket) {
     client.send(this.messageFormat(EventTypes.AUTH_FAILED, '认证失败'));
     client.disconnect();
   }
+
   async authToken(token: string): Promise<boolean> {
     if (typeof token !== 'string') {
       return false;
@@ -60,11 +60,22 @@ export class AdminEventsGateway
       return this.authFailed(client);
     }
 
-    this.wsClients.push(client);
     super.handleConnect(client);
+  }
+
+  @SubscribeMessage('unlog')
+  unsubscribeStdOut(client: Socket) {
+    const idx = this.subscribeStdOutClient.findIndex(
+      (client_) => client_ === client,
+    );
+    Logger.debug(chalk.yellow(client.id, idx));
+    if (~idx) {
+      this.subscribeStdOutClient.splice(idx, 1);
+    }
   }
   handleDisconnect(client: SocketIO.Socket) {
     super.handleDisconnect(client);
+    this.unsubscribeStdOut(client);
   }
 
   handleTokenExpired(token: string) {
@@ -79,6 +90,46 @@ export class AdminEventsGateway
       }
       return false;
     });
+  }
+
+  subscribeStdOutClient: Socket[] = [];
+
+  @SubscribeMessage('log')
+  async subscribeStdOut(client: Socket) {
+    if (
+      this.subscribeStdOutClient.includes(client) ||
+      this.subscribeStdOutClient.some((client_) => client_.id === client.id)
+    ) {
+      return;
+    }
+    this.subscribeStdOutClient.push(client);
+    Logger.debug(
+      chalk.yellow(client.id, this.subscribeStdOutClient.length),
+      'SubscribeStdOut',
+    );
+  }
+
+  bindStdOut() {
+    const handler = (data: any) => {
+      this.subscribeStdOutClient.forEach((client) => {
+        client.send(this.messageFormat(EventTypes.STDOUT, data));
+      });
+    };
+    const stream = {
+      stdout: process.stdout.write,
+      stderr: process.stderr.write,
+    };
+
+    process.stdout.write = function (...rest: any[]) {
+      handler(rest[0]);
+
+      return stream.stdout.apply(this, rest);
+    };
+    process.stderr.write = function (...rest: any[]) {
+      handler(rest[0]);
+
+      return stream.stderr.apply(this, rest);
+    };
   }
 
   sendNotification({
