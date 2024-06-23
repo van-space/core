a7046683b86cf9befe74fb182c2fa8a5dbe31513
huangyan321 <1604549268@qq.com>
2024-06-23 23:30:14 +0800

    fix: search route

---
+++ apps/core/src/modules/search/search.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import {
+  IsEnum,
+  IsIn,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { PagerDto } from '../../shared/dto/pager.dto';
+
+export class SearchDto extends PagerDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  keyword: string;
+
+  @IsString()
+  @ApiProperty({ description: '根据什么排序', required: false })
+  @IsNotEmpty()
+  @IsOptional()
+  orderBy: string;
+
+  @Transform(({ value: val }) => parseInt(val))
+  @IsEnum([1, -1])
+  @IsOptional()
+  @ApiProperty({ description: '倒序|正序', enum: [1, -1], required: false })
+  order: number;
+
+  @IsOptional()
+  @IsIn([0, 1])
+  // HINT: only string type in query params
+  @Transform(({ value }) => (value === 'true' || value === '1' ? 1 : 0))
+  rawAlgolia?: boolean;
+}
