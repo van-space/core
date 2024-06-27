ca714c33778499817edbf65333231b34fd5bf50e
huangyan321 <1604549268@qq.com>
2024-06-27 14:32:19 +0800

    fix: node type error

--- apps/core/src/modules/tool/tool.controller.spec.ts
+++ apps/core/src/modules/tool/tool.controller.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ToolController } from './tool.controller';
-
+import { ToolService } from './tool.service';
+import { ConfigsService } from '../configs/configs.service';
+import { registerGlobal } from 'test/register-global';
+import { HttpService } from '~/processors/helper/helper.http.service';
+import { CacheService } from '~/processors/cache/cache.service';
+registerGlobal();
 describe('ToolController', () => {
   let controller: ToolController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ToolController],
+      providers: [
+        ToolService,
+        {
+          provide: ConfigsService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: HttpService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: CacheService,
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     controller = module.get<ToolController>(ToolController);
--- apps/core/src/modules/tool/tool.service.spec.ts
+++ apps/core/src/modules/tool/tool.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ToolService } from './tool.service';
+import { HttpService } from '~/processors/helper/helper.http.service';
+import { ConfigsService } from '../configs/configs.service';
+import { CacheService } from '~/processors/cache/cache.service';
 
 describe('ToolService', () => {
   let service: ToolService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ToolService],
+      providers: [
+        ToolService,
+        {
+          provide: ConfigsService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: HttpService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: CacheService,
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     service = module.get<ToolService>(ToolService);
