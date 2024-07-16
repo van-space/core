9697a7ec4cbd7ebdaa82ba612e88f931d1ad4b54
huangyan321 <1604549268@qq.com>
2024-07-16 11:04:45 +0800

    chore: change remote

--- apps/core/assets-push.sh
+++ apps/core/assets-push.sh
@@ -5,7 +5,7 @@ cd ../../assets
 git add . || true
 git commit -m 'update assets' || true
 git pull --rebase
-git remote add origin git@github.com:mx-space/assets.git || true
+git remote add origin git@github.com:van-space/assets.git || true
 # git branch -M master
 git push -u origin master -f
 # rm -rf .git
